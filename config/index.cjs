499f043343ad29a4f9a02937a3e4ecc154ea22c7
jkkkkkkkk889 <85441629+jkkkkkkkk889@users.noreply.github.com>
2022-11-04 13:06:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1cwX6LxzituWgc4EVN91otqBf3w',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'o1K6iGFCn019Ww0s7DseaJwylEcqTWp0i7YRHuhNNWI',
+      useTemplateId: 'DFQ-JRux9vsV5c7WvtoJmg68vjUDeCyQrnX6oLZU6r8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
